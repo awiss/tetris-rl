23c1a01c7d93c25abafaf73d26060e3a46529319
lighthouse1029 <jbugli02@villanova.edu>
2014-11-17 00:56:43 -0500

    done ?

--- rl.py
+++ rl.py
@@ -1,3 +1,29 @@
 __author__ = 'Jake Buglione and Alex Wissman'
 
-from pybrain.rl.environments.TetroTask import
+from pybrain.rl.environments.TetroTask import TetroTask
+from pybrain.rl.environments.TetroEnv import TetroEnv
+from pybrain.rl.learners.valuebased import ActionValueTable
+from pybrain.rl.agents import LearningAgent
+from pybrain.rl.learners import Q
+from pybrain.rl.experiments import Experiment
+from pybrain.rl.explorers import EpsilonGreedyExplorer
+
+# Initialize state space
+table = ActionValueTable(2**15, 16)
+table.initialize(0.)
+
+# create learner and agent
+learner = Q(.5, 0.0)
+learner._setExplorer(EpsilonGreedyExplorer(0.0))
+agent = LearningAgent(table, learner)
+
+# create environment, task, and experiment
+env = TetroEnv()
+task = TetroTask(env)
+experiment = Experiment(task, agent)
+
+# Train the learner
+While True:
+	experiment.doInteractions(1)
+	agent.learn()
+	agent.reset()
